9f1f778ec71555a37d2212d05805eecdfa444a50
KennyThrug <kennyepstein6@gmail.com>
2023-11-04 21:53:04 -0500

    User Login, incomplete but functional
    Currently doesn't look good on the User Information Page, but otherwise pushing for functionality
    
    Also Fixed some Merge conflicts

--- assignment02/src/App.js
+++ assignment02/src/App.js
@@ -5,6 +5,8 @@ import Products from "./Products.json";
 import { Categories } from "./Categories";
 
 const cartArray = [];
+let is_logged_in = false;
+let login_info;
 
 function get_search_bar() {
   let x = document.getElementById("SearchBar");
@@ -16,8 +18,7 @@ function get_search_bar() {
   return x.value;
 }
 
-function update_cart(Cart, setCart, PageId, product, setPageId)
-{
+function update_cart(Cart, setCart, PageId, product, setPageId) {
   console.log("Adding " + product.id + " to cart");
   let x = Cart;
   let y = PageId;
@@ -25,19 +26,16 @@ function update_cart(Cart, setCart, PageId, product, setPageId)
   setCart(x);
 }
 
-function removeFromCart(Cart, setCart, index, PageId, setPageId)
-{
+function removeFromCart(Cart, setCart, index, PageId, setPageId) {
   console.log("Removing idx " + index + " from cart");
   let x = Cart;
   let y = PageId;
   x.splice(index, 1);
   setCart(x);
-  if(y == 2)
-  {
+  if (y == 2) {
     setPageId(3);
   }
-  else
-  {
+  else {
     setPageId(2);
   }
 }
@@ -47,7 +45,7 @@ function render_products(ProductsCategory, searchBar, setSearchBar, Cart, setCar
     <div className="category-section fixed">
       <div className="topnav">
         <input onChange={() => setSearchBar(get_search_bar())}
-          type="text" placeholder="Search.." id="SearchBar" style={{ border: "solid", color: "black",  width: "75%"}}></input>
+          type="text" placeholder="Search.." id="SearchBar" style={{ border: "solid", color: "black", width: "75%" }}></input>
       </div>
       <h2 className="text-3xl font-extrabold tracking-tight text-gray-600 category-title">
         Products ({ProductsCategory.length})
@@ -94,6 +92,26 @@ function render_products(ProductsCategory, searchBar, setSearchBar, Cart, setCar
     </div>
   );
 };
+function login_user(setState) {
+  login_info = {
+    Name: document.getElementById("Name").value,
+    Address: document.getElementById("Address").value,
+    State: document.getElementById("State").value,
+    City: document.getElementById("City").value,
+    Zip: document.getElementById("Zip").value,
+    CreditCard: document.getElementById("ccn").value,
+    ExperationDate: document.getElementById("ExperationDate").value,
+    CCV: document.getElementById("CCV").value,
+    NameOnCard: document.getElementById("NameOnCard").value,
+  }
+  is_logged_in = true;
+  setState(1);
+}
+function logout_user(setPageId) {
+  login_info = null;
+  is_logged_in = false;
+  setPageId(1);
+}
 
 const sum_prices = (Cart) =>
 {
@@ -105,6 +123,86 @@ const sum_prices = (Cart) =>
   return sum;
 }
 
+function render_user_login(setState) {
+  return (
+    <section style={{ position: "fixed", marginLeft: "10%", width: "50%" }}>
+      <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+              Sign in to your account
+            </h1>
+            <form class="space-y-4 md:space-y-6" action="#">
+              <div>
+                <label for="Name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Full Name</label>
+                <input type="email" name="Name" id="Name" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="John Doe" required=""></input>
+              </div>
+              <div>
+                <label for="Address" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Address</label>
+                <input type="email" name="Address" id="Address" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="515 Morrill Rd" required=""></input>
+              </div>
+              <div className="columns-3">
+                <div>
+                  <label for="State" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">State</label>
+                  <input type="State" name="State" id="State" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Iowa" required=""></input>
+                </div>
+                <div>
+                  <label for="City" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">City</label>
+                  <input type="City" name="City" id="City" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Ames" required=""></input>
+                </div>
+                <div>
+                  <label for="Zip" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Zip Code</label>
+                  <input type="Zip" name="Zip" id="Zip" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="50011" required=""></input>
+                </div>
+              </div>
+              <div>
+                <label for="ccn" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Credit Card</label>
+                <input id="ccn" type="tel" inputmode="numeric" pattern="[0-9\s]{13,19}" autocomplete="cc-number" maxlength="19" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="xxxx xxxx xxxx xxxx"></input>
+              </div>
+              <div className="columns-3">
+                <div>
+                  <label for="ExperationDate" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Experation Date</label>
+                  <input type="email" name="ExperationDate" id="ExperationDate" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="MM/YY" required=""></input>
+                </div>
+                <div>
+                  <label for="CCV" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">CCV</label>
+                  <input type="email" name="CCV" id="CCV" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="***" required=""></input>
+                </div>
+                <div>
+                  <label for="NameOnCard" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name on Card</label>
+                  <input type="email" name="NameOnCard" id="NameOnCard" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="John Doe" required=""></input>
+                </div>
+              </div>
+              <div class="flex items-center justify-between">
+                <div class="flex items-start">
+                </div>
+              </div>
+              <button type="submit" onClick={() => login_user(setState)} class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+function get_last_four(card_num) {
+  return card_num.substring(card_num.length - 4);
+}
+function render_user_information() {
+  return (
+      <div>
+        <p>
+          Name: {login_info.Name}
+        </p>
+        <p>
+          Address: {login_info.Address}, {login_info.State} {login_info.City}, {login_info.Zip}
+        </p>
+        <p>
+          Credit Card: {login_info.NameOnCard}: Card Ending in {get_last_four(login_info.CreditCard)}
+        </p>
+      </div>
+  )
+}
 
 const render_cart = (Cart, setCart, PageId, setPageId) => {
   return (
@@ -131,7 +229,31 @@ const render = (ProductsCategory, PageId, SearchBar, setSearchBar, Cart, setCart
   } else if (PageId == 2 || PageId == 3) {
     return render_cart(Cart, setCart, PageId, setPageId);
   }
+  else if (PageId == 4) {
+    return render_user_login(setPageId);
+  }
+  else if (PageId == 5) {
+    return render_user_information();
+  }
 };
+
+
+//For Testing Purposes, remove before turning in
+function testLogin(setPageId) {
+  login_info = {
+    Name: "John Doe",
+    Address: "515 Morrill Rd",
+    State: "Iowa",
+    City: "Ames",
+    Zip: "50011",
+    CreditCard: "0000-0000-0000",
+    ExperationDate: "10/31",
+    CCV: "111",
+    NameOnCard: "John Doe",
+  }
+  is_logged_in = true;
+  setPageId(5);
+}
 
 const App = () => {
   const [PageId, setPageId] = useState(1);
@@ -139,6 +261,45 @@ const App = () => {
   const [ProductsCategory, setProductsCategory] = useState(Products);
   const [Cart, setCart] = useState([]);
 
+  let login_buttons;
+  if (is_logged_in) {
+    login_buttons = (
+      <div style={{ marginTop: "-20px" }}>
+        <br></br>
+        <br></br>
+        <button
+          onClick={() => logout_user(setPageId)}
+          className="inline-block bg-amber-600 rounded-full px-3 py-1"
+        >Log Out</button>
+        <br></br>
+        <br></br>
+        <button
+          onClick={() => setPageId(5)}
+          className="inline-block bg-amber-600 rounded-full px-3 py-1"
+        >User Information</button>
+      </div>
+    )
+  }
+  else {
+    login_buttons = (
+      <div style={{ marginTop: "-20px" }}>
+        <br></br>
+        <br></br>
+        <button
+          onClick={() => setPageId(4)}
+          className="inline-block bg-amber-600 rounded-full px-3 py-1"
+        >Log In</button>
+        <div style={{ marginTop: "-20px" }}>
+          <br></br>
+          <br></br>
+          <button
+            onClick={() => testLogin(setPageId)}
+            className="inline-block bg-amber-600 rounded-full px-3 py-1"
+          >Test Login</button>
+        </div>
+      </div>
+    )
+  }
   return (
     <div className="flex fixed flex-row" id="all_page">
       <div
@@ -170,6 +331,7 @@ const App = () => {
           >
             View Catalogue
           </button>
+          {login_buttons}
         </div>
       </div>
       <div className="ml-5 p-10 xl:basis-4/5">
